f9c4bae9a77bd12d694ebddc3289433a536462d6
Oleg G.Kapranov <ok@pure-agency.co>
2020-10-08 03:39:04 -1000

    updated mix.exs

--- mix.exs
+++ mix.exs
@@ -6,6 +6,7 @@ defmodule ElixirAnalyser.MixProject do
       app: :elixir_analyser,
       version: "0.1.0",
       elixir: "~> 1.10",
+      elixirc_paths: elixirc_paths(Mix.env()),
       start_permanent: Mix.env() == :prod,
       deps: deps()
     ]
@@ -13,11 +14,16 @@ defmodule ElixirAnalyser.MixProject do
 
   def application do
     [
-      extra_applications: [:logger]
+      extra_applications: [:logger, :ex_unit]
     ]
   end
 
   defp deps do
-    []
+    [
+      {:jason, "~> 1.2"}
+    ]
   end
+
+  defp elixirc_paths(:test), do: ["lib", "test/support"]
+  defp elixirc_paths(_), do: ["lib"]
 end
