a681e31a6d698cab49e0b46f495f78203312c572
Oleg G.Kapranov <ok@pure-agency.co>
2020-10-08 03:42:37 -1000

    updated formatter.exs

--- .formatter.exs
+++ .formatter.exs
@@ -1,3 +1,12 @@
 [
-  inputs: ["{mix,.formatter}.exs", "{config,lib,test}/**/*.{ex,exs}"]
+  inputs: ["{mix,.formatter}.exs", "{config,lib,test}/**/*.{ex,exs}"],
+  locals_without_parens: [
+    status: :*,
+    find: :*,
+    on_fail: :*,
+    comment: :*,
+    form: :*,
+    suppress_if: :*,
+    depth: :*
+  ]
 ]
